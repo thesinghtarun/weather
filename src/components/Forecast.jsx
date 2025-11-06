857a0b7236e0e3a29ae71fe8f2bb80117011a15d
thesinghtarun <154118242+thesinghtarun@users.noreply.github.com>
2025-11-06 23:57:44 +0530

    Updated forecast temperature

--- src/components/Forecast.jsx
+++ src/components/Forecast.jsx
@@ -3,7 +3,7 @@ export function Forecast({ data }) {
     <div className='border rounded-lg p-3 my-2 bg-gray-100 flex justify-between'>
       <div>{new Date(data.dt*1000).toLocaleDateString([],{hour:'2-digit',minute:'2-digit'})}</div>
       <div>{data.weather[0].description}</div>
-      <div>{data.main.temp.toFixed(1)}°C</div>
+      <div>{(data.main.temp-273.15).toFixed(1)}°C</div>
     </div>
   )
 }
